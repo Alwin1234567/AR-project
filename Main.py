174a9052ff5e89a37316f33c28adc90879230e15
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-02-17 12:10:44 +0100

    basis structuur van main.py toegevoegd

---
+++ Main.py
@@ -0,0 +1,13 @@
+"""
+Header
+Hier komen alle libraries die in het programma gebruikt worden
+"""
+import xlwings as xw
+import datetime as dt
+import matplotlib.pyplot as plt
+
+
+"""
+Body
+Hier komen alle functies
+"""
