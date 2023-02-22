65016229300c26154deeefa0bba0d65e376fc9a8
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-02-22 12:51:06 +0100

    Commentaar bij alle afbeelding functies toegevoegd

--- Main.py
+++ Main.py
@@ -245,6 +245,20 @@ def vergelijken_afbeelding_generatie():
     
 
 def getaltotijd(getal):
+    """
+    Functie die een float met daarin een jaartal en een maand omzet in een string die het mooier weergeeft
+
+    Parameters
+    ----------
+    getal : Float
+        variabele met daarin een jaar en maand.
+
+    Returns
+    -------
+    tijd : String
+        variabele waarin de jaar en maand gescheiden zijn in format XXj XXm.
+
+    """
     jaar = int(getal)
     maand = round((getal - jaar) * 12)
     tijd = "{}j".format(jaar)
@@ -254,6 +268,26 @@ def getaltotijd(getal):
 def getaltogeld(getal): return "€{:.2f}".format(float(getal)).replace(".",",")
 
 def blokkentellen(beginrij, beginkolom, blokafstand, sheet):
+    """
+    Een functie die het aantal blokken met OP of PP informatie telt
+
+    Parameters
+    ----------
+    beginrij : Int
+        De rij vanaf waar het moet gaan rekenen.
+    beginkolom : Int
+        De kolom vanaf waar het moet gaan rekenen.
+    blokafstand : Int
+        De afstand tussen twee blokken.
+    sheet : Book.Sheet Type
+        De Sheet waarop de blokken staan.
+
+    Returns
+    -------
+    blokaantal : Int
+        De hoeveelheid blokken die het algoritme geteld heeft.
+
+    """
     blokaantal = 0
     leescell = [beginrij, beginkolom]
     while sheet.range(tuple(leescell)).value != None:
@@ -262,9 +296,23 @@ def blokkentellen(beginrij, beginkolom, blokafstand, sheet):
     return blokaantal
 
 def kleurinvoer(kleur):
+    """
+    Een functie die een string met rgb waardes veranderd naar een tuple met rgb waardes.
+
+    Parameters
+    ----------
+    kleur : String
+        Bevat drie rgb waardes in een string gescheiden met een ",".
+
+    Returns
+    -------
+    tuple(kleuren) : Tuple(List)
+        De drie rgb waardes als integer in een tuple.
+
+    """
     rgb = kleur.split(",")
     kleuren = list()
-    for i in range(3):
+    for i in range(len(rgb)):
         kleuren.append(int(rgb[i])/255)    
     return tuple(kleuren)
     
