6a1258b88f8e4f519a4d833c7f01281b5a3d44d3
DESKTOP-G5GQ70G\Eric van Huizen <ericvanhuizen2000@gmail.com>
2023-03-06 13:03:28 +0100

    Van alles en nog wat

--- Main.py
+++ Main.py
@@ -144,9 +144,6 @@ def invoer_test_klikken():
     #sheets en book opslaan in variabelen
     book = xw.Book.caller()
     invoer = book.sheets["Tijdelijk invoerscherm"]
-    sterftetafel= book.sheets["Sterftetafels"] 
-    AG2020= book.sheets["AG2020"]
-    pensioencontracten= book.sheets["Gegevens pensioencontracten"]
     
     kolommen= invoer.range((1,8), (61,104))
     #Kolommen legen waar de berekeningen komen
@@ -157,7 +154,9 @@ def invoer_test_klikken():
     sterftetafel_range= invoer.range((10,3),(18,3))
     rentes= invoer.range((10,4),(18,4))
     pensioenleeftijd_range= invoer.range((10,5),(18,5))
-    koopsom_range= invoer.range((10,6),(18,6))
+    koopsomfactor_range= invoer.range((10,6),(18,6))
+    
+    basis_koopsom= invoer.range((10,7),(18,7))
 
     
     pensioenleeftijd=[]
@@ -177,15 +176,15 @@ def invoer_test_klikken():
     counter=1
     for i in range(1,10):
 
-        if pensioenbedragen(i).value != None:
-            kolom_t= (counter-1)*10+8
-            kolom_jaar= (counter-1)*10+9
-            kolom_leeftijd= (counter-1)*10+10
-            kolom_tpx= (counter-1)*10+11
-            kolom_tqx= (counter-1)*10+12
-            kolom_tqx_juli= (counter-1)*10+13
-            kolom_dt= (counter-1)*10+14
-            kolom_dt_juli= (counter-1)*10+15
+        if pensioenbedragen(i).value != None and pensioenbedragen(i).value != 0:
+            kolom_t= (counter-1)*10+9
+            kolom_jaar= (counter-1)*10+10
+            kolom_leeftijd= (counter-1)*10+11
+            kolom_tpx= (counter-1)*10+12
+            kolom_tqx= (counter-1)*10+13
+            kolom_tqx_juli= (counter-1)*10+14
+            kolom_dt= (counter-1)*10+15
+            kolom_dt_juli= (counter-1)*10+16
             
 
             rente.append(rentes(i).value)
@@ -226,34 +225,101 @@ def invoer_test_klikken():
             else:
                 invoer.range((1, kolom_tpx)).value= "tpx"
                 invoer.range((2, kolom_tpx), (61, kolom_tpx)).formula= [['=if(' + letters[kolom_leeftijd-1] + '2<>"", INDEX(INDIRECT($C$' + str(i+9) + '),' + letters[kolom_leeftijd-1] + '2+1,1)/ INDEX(INDIRECT($C$' + str(i+9) + '),$' + letters[kolom_leeftijd-1] + '$2+1,1),"")']]
+    
+            
+            if regeling_range(i).value== "ZL":
+                koopsomfactor_range(i).value= 0
+                basis_koopsom(i).value= pensioenbedragen(i).value
+                
+            elif "OP" in regeling_range(i).value:
+                koopsomfactor_range(i).formula= [['=SUMPRODUCT(' + letters[kolom_tpx-1] + '2:' + letters[kolom_tpx-1] + '61,' + letters[kolom_dt-1] + '2:' + letters[kolom_dt-1] + '61)']]
+                basis_koopsom(i).value= pensioenbedragen(i).value*koopsomfactor_range(i).value
+                
+            else:
+                koopsomfactor_range(i).formula= [['=SUMPRODUCT(' + letters[kolom_tpx-1] + '2:' + letters[kolom_tpx-1] + '61,' + letters[kolom_tqx_juli-1] +'2:' + letters[kolom_tqx_juli-1] + '61,'+ letters[kolom_dt_juli-1] + '2:' + letters[kolom_dt_juli-1] + '61)']]
+                basis_koopsom(i).value= pensioenbedragen(i).value*koopsomfactor_range(i).value
+            
             
             counter+= 1
             
-            if regeling_range(i).value== "ZL":
-                koopsom_range(i).formula= [['=$B' + str(i+9)]]
-                
-            elif "OP" in regeling_range(i).value:
-                koopsom_range(i).formula= [['=SUMPRODUCT(' + letters[kolom_tpx-1] + '2:' + letters[kolom_tpx-1] + '61,' + letters[kolom_dt-1] + '2:' + letters[kolom_dt-1] + '61)*B' + str(i+9) ]]
-            
-            else:
-                koopsom_range(i).formula= [['=SUMPRODUCT(' + letters[kolom_tpx-1] + '2:' + letters[kolom_tpx-1] + '61,' + letters[kolom_tqx_juli-1] +'2:' + letters[kolom_tqx_juli-1] + '61,'+ letters[kolom_dt_juli-1] + '2:' + letters[kolom_dt_juli-1] + '61)*B' + str(i+9)]]
-                
-            
         else:
+            basis_koopsom(i).value= 0
             rente.append(0)
             pensioenleeftijd.append(0)
             
-            
-            
-            
-        
-            
-            
 
+           
         
-    
+            
+@xw.sub
+def flexibilisaties_testen():
+   book = xw.Book.caller()
+   invoer = book.sheets["Tijdelijk invoerscherm"] 
+   
+   flexibilisaties= invoer.range((21,2), (55,6))
+   
+   regeling_range= invoer.range((10,1), (18,1))
+   pensioenleeftijd= invoer.range((10,5), (18,5))
+   koopsomfactor_range= invoer.range((10,6), (18,6))
+   basis_koopsom= invoer.range((10,7), (18,7))
 
-        
-        
-    
-    
+   koopsommen= []
+   koopsommen= basis_koopsom.value
+   
+   for i in range(1,10):
+       regeling= (i-1)*8+1
+       soort= (i-1)*8+2
+       hoeveelheid= (i-1)*8+3
+       factor= (i-1)*8+4
+       aanspraak_OP= (i-1)*8+5
+       aanspraak_PP= (i-1)*8+6
+       
+       for c in range(1,5):
+           if flexibilisaties(regeling, c).value!= None:
+               
+               
+               
+               
+               
+               
+               
+               
+           # if flexibilisaties(regeling).value[:-2] in invoer.range((11,1)).value and flexibilisaties(regeling).value[-2:] in invoer.range((11,1)).value:
+           #     print("test")
+           #     # if flexibilisaties(2).value== "vervroegen" or flexibilisaties(2).value== "verlaten":
+           #     #     #Bij corresponderende pensioenregelingsrij de hoeveelheid vervroegen/verlaten optellen
+           #     #     pensioenleeftijd(2).value= pensioenleeftijd(2).value+ flexibilisaties(3).value
+                   
+                   
+                   
+           #     # elif  flexibilisaties(2).value== "uitruilen":
+           #     #     print("Uitruilen")
+                   
+           #     # elif flexibilisaties(2).value== "hoog" or flexibilisaties(2).value== "laag":
+           #     #      print("Hoogconstructie of laagconstructie") 
+                    
+           #     # else:
+           #     #     print("AOW overbruggen")
+                   
+           # else:
+           #     print("Het werkt niet")
+       
+       
+       
+       
+       
+       
+       
+   
+
+   
+   
+   
+       
+       
+       
+       
+       
+       
+       
+       
